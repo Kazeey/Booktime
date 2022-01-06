04c0fc77ff12c0dbb5d9ec2a89cdee90835c0d1a
Kazeey <44819132+Kazeey@users.noreply.github.com>
2022-01-06 21:56:29 +0100

    Add container's test
    Add a test for a container's test

---
+++ front-web/src/components/link/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Link from './index';
+
+test('Link changes the class when hovered', () => {
+  const component = renderer.create(<Link page="http://www.google.com">Google</Link>,);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  tree.props.onMouseEnter();
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  tree.props.onMouseLeave();
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
