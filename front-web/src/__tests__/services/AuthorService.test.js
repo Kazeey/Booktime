0dceb6081ca4dc694befd7a07302c51f111eda1a
Quentin Pezé <44819132+Kazeey@users.noreply.github.com>
2022-01-10 16:22:48 +0100

    Test mongodb in memory

--- front-web/src/__tests__/services/AuthorService.test.js
+++ front-web/src/__tests__/services/AuthorService.test.js
@@ -1,23 +1,66 @@
 import { getAuthorsList } from "../../services/AuthorService";
 
+import { MongoClient } from "mongodb";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+const author = [
+    {
+        id : "1",
+        name : "Joanne",
+        firstname : "Rowling",
+        birthdate : "1965-07-31",
+        deathdate : "",
+        country : "UK",
+        biography : "Joanne Rowling is a British novelist, screenwriter and film producer. She is best known for her Harry Potter novels, which have won multiple awards and sold more than 500 million copies, and for her eponymous children's book series, which has been translated into fifty-eight languages and sold more than 500 million copies. She is also the author of the Harry Potter film series, which has won multiple awards and sold more than 80 million copies. She has also written the biography of J.K. Rowling, which has been adapted into a number of other media, and was the basis for the Harry Potter film series. She has also written a number of non-fiction books, most notably The Casual Vacancy, which was published in 2008."
+    }
+]
+
 describe("AuthorService", () => {
-    describe("API call is successfull", () => {
-        it("should return authors list", done => {
-            getAuthorsList()
-            .then(response => {
-                expect(response).toEqual(
-                    expect.objectContaining({
-                        id : expect.any(Number),
-                        name : expect.any(String),
-                        firstname : expect.any(String),
-                        birthdate : expect.any(String),
-                        city : expect.any(String),
-                        country : expect.any(String),
-                        image : expect.any(String)
-                    })
-                )
-                done();
-            });
-        });
+    let con;
+    let mongoServer;
+
+    beforeAll(async () => {
+        mongoServer = await MongoMemoryServer.create();
+        con = await MongoClient.connect(mongoServer.getUri(), {});
+    });
+
+    afterAll(async () => {
+        if (con)
+            await con.close();
+        if (mongoServer)
+            await mongoServer.stop();
+    });
+
+    if("Should return authors list", async () => {
+        const db = con.db("test");
+
+        expect(db).toBeDefined();
+        const col = db.collection("authors");
+        const result = await col.insertOne(author);
+        expect(result.insertedCount).toBe(1);
+        expect(await col.countDocuments({})).toBe(1);
+        expect(await col.find({}).toArray()).toEqual(author);
     });
 });
+
+// describe("AuthorService", () => {
+//     describe("API call is successfull", () => {
+//         it("should return authors list", done => {
+//             getAuthorsList()
+//             .then(response => {
+//                 expect(response).toEqual(
+//                     expect.objectContaining({
+//                         id : expect.any(Number),
+//                         name : expect.any(String),
+//                         firstname : expect.any(String),
+//                         birthdate : expect.any(String),
+//                         city : expect.any(String),
+//                         country : expect.any(String),
+//                         image : expect.any(String)
+//                     })
+//                 )
+//                 done();
+//             });
+//         });
+//     });
+// });
