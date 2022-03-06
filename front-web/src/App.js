d958dec3c35a87cd19d69eece363206af6bc582c
Kazeey <44819132+Kazeey@users.noreply.github.com>
2022-03-06 18:08:28 +0100

    Add modal & authentication
    Creation of the modal component - ongoing

--- front-web/src/App.js
+++ front-web/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
+import Modal from './components/modal';
 
 
 const App = () => {
   return (
     <div className="App">
-            
+      App
+      <Modal />
     </div>
   )
 }
