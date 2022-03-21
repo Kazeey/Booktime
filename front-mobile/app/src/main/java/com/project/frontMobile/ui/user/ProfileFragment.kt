709dae033457c559d482376a7841ab7a8330375f
EliottDupauDKT <92303075+EliottDupauDKT@users.noreply.github.com>
2022-03-21 16:21:27 +0100

    Create dialog custom view

--- front-mobile/app/src/main/java/com/project/frontMobile/ui/user/ProfileFragment.kt
+++ front-mobile/app/src/main/java/com/project/frontMobile/ui/user/ProfileFragment.kt
@@ -1,12 +1,15 @@
 package com.project.frontMobile.ui.user
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.project.frontMobile.R
@@ -48,29 +51,27 @@ class ProfileFragment : Fragment(), ClickHandler {
     override fun onClick(view: View) {
         when (view.id) {
             binding.usernameContainer.id -> {
-               // openDialog("Username")
+                openDialog()
             }
             binding.birthdayContainer.id  -> {
                 openDatePicker(
                     Integer.parseInt(DateUtils().extractYear(currentUser.birthday)),
                     Integer.parseInt(DateUtils().extractMonth(currentUser.birthday)) - 1,
-                    Integer.parseInt(DateUtils().extractDayOfMonth(currentUser.birthday)))
+                    Integer.parseInt(DateUtils().extractDayOfMonth(currentUser.birthday))
+                )
             }
             else -> Log.i(UserViewModel::class.java.name, "Unknown view")
         }
     }
 
-    /*fun openDialog(title: String) {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle(title)
-            .setPositiveButton("Ok") { dialog, which ->
-                Log.d(UserViewModel::class.java.name, "ok")
-            }
-            .setNegativeButton("Cancel") { dialog, which ->
-                Log.d(UserViewModel::class.java.name, "cancel")
-            }
-            .show()
-    }*/
+    fun openDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setView(R.layout.custom_dialog)
+
+        builder.show()
+
+        //val input = dialog.findViewById<TextView>(R.id.edittext)
+    }
 
     private fun openDatePicker(year: Int, month: Int, day: Int) {
         DatePickerDialog(requireContext(), { _, y, m, d ->
