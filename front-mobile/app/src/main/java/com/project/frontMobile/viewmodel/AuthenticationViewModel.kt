6a92522d7a41f0d362a28944ab8a625cc4375b0b
EliottDupauDKT <92303075+EliottDupauDKT@users.noreply.github.com>
2022-03-31 16:59:32 +0200

    create sign up views

--- front-mobile/app/src/main/java/com/project/frontMobile/viewmodel/AuthenticationViewModel.kt
+++ front-mobile/app/src/main/java/com/project/frontMobile/viewmodel/AuthenticationViewModel.kt
@@ -5,13 +5,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.findNavController
+import com.project.frontMobile.R
 import com.project.frontMobile.ui.authentication.login.LogInFragmentDirections
+import com.project.frontMobile.ui.authentication.signup.SignUpFragmentDirections
 
 class AuthenticationViewModel: ViewModel() {
 
     private val _email = MutableLiveData<String>()
     val email: LiveData<String>
         get() = _email
+
+    private val _confirmEmail = MutableLiveData<String>()
+    val confirmEmail: LiveData<String>
+        get() = _confirmEmail
 
     private val _password = MutableLiveData<String>()
     val password: LiveData<String>
@@ -21,24 +27,23 @@ class AuthenticationViewModel: ViewModel() {
         _email.value = input.trim().toString()
     }
 
+    fun onConfirmEmailChanged(input: CharSequence) {
+        _confirmEmail.value = input.trim().toString()
+    }
+
     fun onPasswordChanged(input: CharSequence) {
         _password.value = input.trim().toString()
     }
 
-    fun signIn(view: View) {
-        if (email.value != null && password.value != null) {
-            val action = LogInFragmentDirections.actionLogInFragmentToLibraryFragment()
-            view.findNavController().navigate(action)
+    fun onClick(view: View) {
+        val action = when (view.id) {
+            R.id.sign_up_button -> SignUpFragmentDirections.actionSignUpFragmentToLibraryFragment()
+            R.id.back_nav_sign_up -> SignUpFragmentDirections.actionSignUpFragmentToAuthenticationFragment()
+            R.id.log_in_button -> LogInFragmentDirections.actionLogInFragmentToLibraryFragment()
+            R.id.forgot_password_button -> LogInFragmentDirections.actionLogInFragmentToForgotPasswordFragment()
+            R.id.back_nav_log_in -> LogInFragmentDirections.actionLogInFragmentToAuthenticationFragment()
+            else -> LogInFragmentDirections.actionLogInFragmentToAuthenticationFragment()
         }
-    }
-
-    fun forgotPassword(view: View) {
-        val action = LogInFragmentDirections.actionLogInFragmentToForgotPasswordFragment()
-        view.findNavController().navigate(action)
-    }
-
-    fun backPressed(view: View) {
-        val action = LogInFragmentDirections.actionLogInFragmentToAuthenticationFragment()
         view.findNavController().navigate(action)
     }
 
