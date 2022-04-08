3180474fbfda94c0f25234a433d803c9e737ba17
EliottDupauDKT <92303075+EliottDupauDKT@users.noreply.github.com>
2022-04-08 09:47:27 +0200

    feat: add catch phrase to create profile screen

--- front-mobile/app/src/main/java/com/project/frontMobile/ui/user/CreateProfileFragment.kt
+++ front-mobile/app/src/main/java/com/project/frontMobile/ui/user/CreateProfileFragment.kt
@@ -2,7 +2,6 @@ package com.project.frontMobile.ui.user
 
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
