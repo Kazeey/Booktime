6d5f076172ed50c2dda49a833c495cab040ab9ce
Kazeey <44819132+Kazeey@users.noreply.github.com>
2022-04-27 23:01:44 +0200

    Update Image.ts

--- back-node/src/utils/Image.ts
+++ back-node/src/utils/Image.ts
@@ -1,11 +1,29 @@
 import { Constants } from "../config";
 import express, { Request, Response } from "express";
+import fs from "fs";
+
+let fetch = require('node-fetch');
 
 export default class ImageConvert
 {
-    public convertImage (url: string): Promise<string>
+    public async convertImage (url: string)
     {
-        
+        return new Promise(async (resolve, reject) => {
+            await fetch(url)
+            .then((response: { ok: any; statusText: any; blob: () => any; }) => {
+                if (!response.ok)
+                    reject(`Error: ${response.statusText}`);
+                return response.blob();
+            })
+            .then((blob: Blob) => {
+                console.log("Avant blob");
+                const reader = fs;
+                let Url = new URL(url);
+                console.log(Url);
+                let contents = reader.readFileSync(Url.href, {encoding: 'base64'});
+                return resolve(contents);
+            });
+        });
     }
 
     public encodeImageFromUrl (req: Request, res: Response)
